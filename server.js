2a96d228b222ef6328c242780d658d78fc3f371e
Scott <scott@ugps-MacBook-Pro.local>
2018-05-16 14:52:05 -0600

    first prod commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path = require("path");
 var app = express();
+var port = process.env.PORT || "3000"
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen("3000", function() { console.log("port 3k")});
+app.listen(port, function() { console.log("port 3k")});
